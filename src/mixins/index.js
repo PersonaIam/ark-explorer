595781ea8e6ad4a3c9269ec6d7577038349cf290
Georgi Stoyanov <33185290+geops22@users.noreply.github.com>
2018-05-17 16:21:01 -0500

    Fix Epoch

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -23,10 +23,10 @@ const methods = {
     return moment()
       .utc()
       .set({
-        year: 2017,
-        month: 2,
-        date: 21,
-        hour: 13,
+        year: 2018,
+        month: 1,
+        date: 1,
+        hour: 0,
         minute: 0,
         second: 0,
       })
@@ -39,10 +39,10 @@ const methods = {
     return moment()
       .utc()
       .set({
-        year: 2017,
-        month: 2,
-        date: 21,
-        hour: 13,
+        year: 2018,
+        month: 1,
+        date: 1,
+        hour: 0,
         minute: 0,
         second: 0,
       })
