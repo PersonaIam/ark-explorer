d73b9140e5ecf05615ed94ef366987237978ac9e
remus-persona <remus@persona.im>
2019-02-04 14:26:34 +0200

    add attribute related types. Force testnet to run on https

--- src/components/links/Wallet.vue
+++ src/components/links/Wallet.vue
@@ -13,9 +13,27 @@
       <span v-else-if="type === 3">{{ $t("Vote") }}</span>
       <span v-else-if="type === 4">{{ $t("Multisignature Registration") }}</span>
       <span v-else-if="type === 5">{{ $t("IPFS") }}</span>
-      <span v-else-if="type === 6">{{ $t("Timelock Transfer") }}</span>
-      <span v-else-if="type === 7">{{ $t("Multipayment") }}</span>
-      <span v-else-if="type === 8">{{ $t("Delegate Resignation") }}</span>
+      <span v-else-if="type === 6">{{ $t("Register") }}</span>
+      <span v-else-if="type === 7">{{ $t("Verify") }}</span>
+      <span v-else-if="type === 8">{{ $t("Create Attribute") }}</span>
+      <span v-else-if="type === 9">{{ $t("Update Attribute") }}</span>
+      <span v-else-if="type === 10">{{ $t("Request Attribute Validation") }}</span>
+      <span v-else-if="type === 11">{{ $t("Approve Attribute Validation Request") }}</span>
+      <span v-else-if="type === 12">{{ $t("Decline Attribute Validation Request") }}</span>
+      <span v-else-if="type === 13">{{ $t("Notarize Attribute Validation Request") }}</span>
+      <span v-else-if="type === 14">{{ $t("Reject Attribute Validation Request") }}</span>
+      <span v-else-if="type === 15">{{ $t("Cancel Attribute Validation Request") }}</span>
+      <span v-else-if="type === 16">{{ $t("Request Identity Use") }}</span>
+      <span v-else-if="type === 17">{{ $t("Approve Identity Use Request") }}</span>
+      <span v-else-if="type === 18">{{ $t("End Identity Use Request") }}</span>
+      <span v-else-if="type === 19">{{ $t("Cancel Identity Use Request") }}</span>
+      <span v-else-if="type === 20">{{ $t("Decline Identity Use Request") }}</span>
+      <span v-else-if="type === 21">{{ $t("Attribute Consumption") }}</span>
+      <span v-else-if="type === 22">{{ $t("Run Reward Round") }}</span>
+      <span v-else-if="type === 23">{{ $t("Reward") }}</span>
+      <span v-else-if="type === 24">{{ $t("Update Reward Round") }}</span>
+      <span v-else-if="type === 25">{{ $t("Create Service") }}</span>
+      <span v-else-if="type === 26">{{ $t("Inactivate Service") }}</span>
     </span>
 
     <span class="md:hidden">
