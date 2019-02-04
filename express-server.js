c5e03aac0449b2a237776ec7ec273a19587db980
remus-persona <remus@persona.im>
2019-02-04 15:01:17 +0200

    replace http with https in express-server

--- express-server.js
+++ express-server.js
@@ -5,6 +5,7 @@ const history = require('connect-history-api-fallback')
 const app = express()
 const ip = process.env.EXPLORER_HOST || '127.0.0.1'
 const port = process.env.EXPLORER_PORT || 4200
+const https = require('https')
 
 // Web Server
 const distPath = path.join(__dirname + '/dist/')
@@ -16,7 +17,7 @@ if (fs.existsSync(distPath)) {
   app.get('/', function (req, res) {
     res.render(path.join(distPath + '/index.html'))
   })
-  const server = app.listen(port, ip)
+  const server = https.createServer({ ... }, app).listen(443);
 } else {
     throw new Error('No Dist Path - please build')
 }
